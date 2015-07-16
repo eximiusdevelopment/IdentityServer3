02deb066239725d4eed1a7a38a65dd38b2f3f941
eximiusdevelopment <eXimius1$>
2015-07-16 12:23:44 +0200

    Update version

--- source/Core/Properties/AssemblyInfo.cs
+++ source/Core/Properties/AssemblyInfo.cs
@@ -38,3 +38,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8fd81f8c-9a47-479e-9a83-bdf66f883d78")]
 //[assembly: InternalsVisibleTo("Thinktecture.IdentityServer.Core.Tests")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
